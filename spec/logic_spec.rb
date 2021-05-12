14542ceef7b05b09f64d14d2c47dbdbeb3df5c7b
Lucky <vagyasri2011@gmail.com>
2021-05-12 18:23:54 +0200

    Describe the Player file

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -1,15 +1,14 @@
-#!/usr/bin/env ruby
-
-require_relative ".lib/logic"
+require './lib/logic'
 
 describe Logic do
-    describe '#initialize' do
-        it 'Fetch the value of :player variable from the check_winner and assign it to @player instance varable' do
-          expect 
-        end
+  describe '#initialize' do
+    it 'Fetch the value of :player variable from the check_winner and assign it to @player instance varable' do
+      expect
     end
-    describe '#winner' do
-        it 'Checks the row/column/diagonal values of the board' do
-          expect 
+  end
+  describe '#winner' do
+    it 'Checks the row/column/diagonal values of the board' do
+      expect
     end
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,18 @@
-#!/usr/bin/env ruby
-
-require_relative ".lib/player"
+require './lib/player'
 
 describe Player do
-    describe '#initialize' do
-        it 'Fetch the value of :name variable from the check_winner and assign it to @name instance varable' do
-          expect 
-        end
+  describe '#initialize' do
+    it 'Fetch the value of :name variable from the check_winner and assign it to @name instance variable' do
+      player = Player.new({ name: 'Lucky', marker: '✖️', spt: [] })
+      expect(player.name).to eq('Lucky')
     end
+    it 'Fetch the value of :maker variable from the check_winner and assign it to @maker instance variable' do
+      player = Player.new({ name: 'Lucky', marker: '✖️', spt: [] })
+      expect(player.marker).to eq('✖️')
+    end
+    it 'Fetch the value of :spt variable from the check_winner and assign it to @spot instance variable' do
+      player = Player.new({ name: 'Lucky', marker: '✖️', spt: [] })
+      expect(player.spt).to eq([])
+    end
+  end
 end
