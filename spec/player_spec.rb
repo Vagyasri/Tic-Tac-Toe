28c9ffec83e3be4c3a0fa67b9b40a1699316343e
Lucky <vagyasri2011@gmail.com>
2021-05-12 16:48:07 +0200

    Set Path for player file
    Rename the spec file
    describe the Player class
    Describe the Initialize Method

---
+++ spec/player_spec.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require_relative ".lib/player"
+
+describe Player do
+    describe '#initialize' do
+        it 'Fetch the value of :name variable from the check_winner and assign it to @name instance varable' do
+          expect 
+        end
+    end
+end
