4998756055694dadac7e59d30f661204c35068ee
Lucky <vagyasri2011@gmail.com>
2021-05-12 16:57:21 +0200

    Create a new file 'logic_spec.rb' inside the Spec folder
    Make file executable
    Set Path for logic file
    describe the logic class
    Describe the Initialize Method
    Describe the Winner Method

---
+++ spec/logic_spec.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require_relative ".lib/logic"
+
+describe Logic do
+    describe '#initialize' do
+        it 'Fetch the value of :player variable from the check_winner and assign it to @player instance varable' do
+          expect 
+        end
+    end
+    describe '#winner' do
+        it 'Checks the row/column/diagonal values of the board' do
+          expect 
+    end
+end
