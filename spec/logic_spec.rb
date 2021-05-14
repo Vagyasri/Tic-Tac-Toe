db2f15b2026b1deb4e2a8499b508e46a5b7c79d1
Lucky <vagyasri2011@gmail.com>
2021-05-14 10:21:42 +0200

    Add spec to Logic

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -2,13 +2,23 @@ require './lib/logic'
 
 describe Logic do
   describe '#initialize' do
-    it 'Fetch the value of :player variable from the check_winner and assign it to @player instance varable' do
-      expect
-    end
+    let (:player_x) {{name: "Lucky"}}
+    context 'when we pass an argument' do
+      it 'we should get the value of the player' do 
+        play = Logic.new({name: "Lucky"})
+        expect(play.player).to eq({name:"Lucky"})
+      end
   end
-  describe '#winner' do
-    it 'Checks the row/column/diagonal values of the board' do
-      expect
-    end
+    context "when we don't pass an argument" do
+      it 'we should raise an error' do 
+        expect { Logic.new }.to raise_error(StandardError)
+      end
   end
+  end
+  #  describe '#winner?' do
+  #    it 'Checks the row/column/diagonal values of the board' do
+  #     win = ["x", "x", "x"].freeze
+  #     expect()
+  #   end
+  #  end
 end
