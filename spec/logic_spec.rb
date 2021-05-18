1dbf780b8401d2eca52ec1182d129e7b4434ed27
Lucky <vagyasri2011@gmail.com>
2021-05-18 15:24:01 +0200

    Fix the rubocop error

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -3,24 +3,24 @@ require './lib/player'
 
 describe Logic do
   describe '#initialize' do
-    let (:player_x) {{name: "Lucky"}}
+    let(:player_x) { { name: 'Lucky' } }
     context 'when we pass an argument' do
-      it 'we should get the value of the player' do 
-        play = Logic.new({name: "Lucky"})
-        expect(play.player).to eq({name:"Lucky"})
+      it 'we should get the value of the player' do
+        play = Logic.new({ name: 'Lucky' })
+        expect(play.player).to eq({ name: 'Lucky' })
       end
     end
     context "when we don't pass an argument" do
-      it 'we should raise an error' do 
+      it 'we should raise an error' do
         expect { Logic.new }.to raise_error(StandardError)
       end
     end
   end
   describe '#winner?' do
     player = Player.new({ name: 'Lucky', marker: '✖️', spt: [1, 2, 3] })
-    it "returns true if WIN1 = player.spt" do
+    it 'returns true if WIN1 = player.spt' do
       logic = Logic.new(player)
       expect(logic.winner?).to eq true
-    end  
+    end
   end
 end
